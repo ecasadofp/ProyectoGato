503dcbf72b745c62653de695fe5a10f5b1111672
Alexis Torres <220240@fppiramide.com>
2022-11-10 13:14:32 +0100

    OwO

--- src/Gato.java
+++ src/Gato.java
@@ -1,3 +1,5 @@
+import java.util.concurrent.TimeUnit;
+
 public class Gato {
 
         private String nombre;
@@ -43,5 +45,29 @@ public class Gato {
                 if (!pelo.equals("Corto")) System.out.println("El gato necesita un corte de pelo");
             }
         }
+    public void saludo() throws InterruptedException {
+        System.out.println("Initializating Artifitial Retardation");
+        System.out.println("|Progress  0.95%|");
+        TimeUnit.SECONDS.sleep(1);
+        System.out.println("|Progress  5.65%|");
+        TimeUnit.SECONDS.sleep(2);
+        System.out.println("|Progress 15.75%|");
+        TimeUnit.SECONDS.sleep(4);
+        System.out.println("|Progress    80%|");
+        TimeUnit.SECONDS.sleep(2);
+        System.out.println("|Progress 99.99%|");
+        TimeUnit.SECONDS.sleep(10);
+        System.out.println("----------------------------------------------------------------");
+        System.out.println("(OwO) whats this?");
+        TimeUnit.SECONDS.sleep(5);
+        System.out.println("Hewwo, w-would you liek to do sumthing with me? :3 <3 uwu");
+        System.out.println("----------------------------------------------------------------");
+        System.out.println("|1º");
+        System.out.println("|2º");
+        System.out.println("|3º");
+        System.out.println("|4º");
+        System.out.println("|5º");
+        System.out.println("----------------------------------------------------------------");
+    }
     }
 
